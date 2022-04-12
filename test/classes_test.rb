1f12672319924f32e9611d314027ff84b33a826d
Omochice-mac <mochice.mls.ntl@gmail.com>
2022-04-12 13:43:52 +0900

    :+1: Add a test for `Multiply`

--- test/classes_test.rb
+++ test/classes_test.rb
@@ -66,3 +66,51 @@ class AddTest < Minitest::Test
     end
   end
 end
+
+class MultiplyTest < Minitest::Test
+  def test_to_s
+    100.times do
+      l = rand(0..1000)
+      r = rand(0..1000)
+      left = Number.new(l)
+      right = Number.new(r)
+      actual = Multiply.new(left, right).to_s
+      assert_equal("#{l} * #{r}", actual)
+    end
+  end
+
+  def test_inspect
+    100.times do
+      l = rand(0..1000)
+      r = rand(0..1000)
+      left = Number.new(l)
+      right = Number.new(r)
+      actual = Multiply.new(left, right).inspect
+      assert_equal("<<#{l} * #{r}>>", actual)
+    end
+  end
+
+  def test_reducible
+    100.times do
+      l = rand(0..1000)
+      r = rand(0..1000)
+      left = Number.new(l)
+      right = Number.new(r)
+      actual = Multiply.new(left, right).reducible?
+      assert(actual)
+    end
+  end
+
+  def test_reduce
+    100.times do
+      l = rand(0..1000)
+      r = rand(0..1000)
+      left = Number.new(l)
+      right = Number.new(r)
+      actual = Multiply.new(left, right).reduce({})
+      assert_instance_of(Number, actual)
+      assert_equal(Number.new(l * r), actual)
+      assert(!actual.reducible?)
+    end
+  end
+end
