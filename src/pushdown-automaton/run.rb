ca956e41f0a4598863f1e6fb24d250d8068421a5
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-23 18:56:36 +0900

    :+1: Add `DPDA` class

--- src/pushdown-automaton/run.rb
+++ src/pushdown-automaton/run.rb
@@ -2,14 +2,6 @@ require_relative "./stack.rb"
 require_relative "./pushdown-automaton-rule.rb"
 require_relative "./deterministic-pushdown-automaton.rb"
 
-rule = PDARule.new(1, "(", 2, "$", ["b", "$"])
-# p rule
-configuration = PDAConfiguration.new(1, Stack.new(["$"]))
-# p configuration
-# p rule.applies_to?(configuration, "(")
-#
-rule.follow(configuration)
-
 rulebook = DPDARulebook.new([
   PDARule.new(1, "(", 2, "$", ["b", "$"]),
   PDARule.new(2, "(", 2, "b", ["b", "b"]),
@@ -17,6 +9,9 @@ rulebook = DPDARulebook.new([
   PDARule.new(2, nil, 1, "$", ["$"]),
 ])
 
-p configuration = rulebook.next_configuration(configuration, "(")
-p configuration = rulebook.next_configuration(configuration, "(")
-p configuration = rulebook.next_configuration(configuration, ")")
+p dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(["$"])), [1], rulebook)
+p dpda.accepting?
+
+dpda.read_string("(()")
+p dpda.accepting?
+p dpda.current_configuration
