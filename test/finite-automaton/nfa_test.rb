ae73c86010c5f10fb79895a5b31441f4467439e3
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-26 22:52:56 +0900

    :hammer: Add test for nfa

--- test/finite-automaton/nfa_test.rb
+++ test/finite-automaton/nfa_test.rb
@@ -34,6 +34,10 @@ class NFARulebookTest < Minitest::Test
     assert_equal(Set[2, 4], rulebook.next_states(Set[1], nil))
     assert_equal(Set[1, 2, 4], rulebook.follow_free_moves(Set[1]))
   end
+
+  def alphabet
+    assert_equal(Set(["a", "b"]), Set(@rulebook.alphabet))
+  end
 end
 
 class NFATest < Minitest::Test
@@ -81,5 +85,44 @@ class NFADesignTest < Minitest::Test
     assert @nfa_design.accepts?("bab")
     assert @nfa_design.accepts?("bbbbb")
     refute @nfa_design.accepts?("bbabb")
+  end
+end
+
+class NFASimulationTest < Minitest::Test
+  def setup
+    rulebook = NFARulebook.new([
+      FARule.new(1, "a", 1), FARule.new(1, "a", 2), FARule.new(1, nil, 2),
+      FARule.new(2, "b", 3),
+      FARule.new(3, "b", 1), FARule.new(3, nil, 2),
+    ])
+    nfa_design = NFADesign.new(1, [3], rulebook)
+    @simulation = NFASimulation.new(nfa_design)
+  end
+
+  def test_next_state
+    assert_equal(Set[1, 2], @simulation.next_state(Set[1, 2], "a"))
+    assert_equal(Set[2, 3], @simulation.next_state(Set[1, 2], "b"))
+    assert_equal(Set[1, 2, 3], @simulation.next_state(Set[2, 3], "b"))
+    assert_equal(Set[1, 2, 3], @simulation.next_state(Set[2, 3], "b"))
+    assert_equal(Set[1, 2], @simulation.next_state(Set[1, 2, 3], "a"))
+  end
+
+  def test_rules_for
+    expected = Set.new([FARule.new(Set[1, 2], "a", Set[1, 2]),
+                        FARule.new(Set[1, 2], "b", Set[2, 3])])
+    actual = Set.new(@simulation.rules_for(Set[1, 2]))
+    assert_equal(expected, actual)
+
+    expected = Set.new([FARule.new(Set[3, 2], "a", Set[]),
+                        FARule.new(Set[3, 2], "b", Set[1, 2, 3])])
+    actual = Set.new(@simulation.rules_for(Set[3, 2]))
+    assert_equal(expected, actual)
+  end
+
+  def test_to_dfa_design
+    dfa_design = @simulation.to_dfa_design
+    refute dfa_design.accepts?("aaa")
+    assert dfa_design.accepts?("aab")
+    assert dfa_design.accepts?("bbbabb")
   end
 end
