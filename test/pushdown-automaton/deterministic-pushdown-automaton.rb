4473554cba55b87ff111a31519960ca7cfc1ee44
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-27 21:34:51 +0900

    :hammer: Add test for PDA

--- test/pushdown-automaton/deterministic-pushdown-automaton.rb
+++ test/pushdown-automaton/deterministic-pushdown-automaton.rb
@@ -58,4 +58,26 @@ class DPDADesignTest < Minitest::Test
     refute @dpda_design.accepts?("(((((((()))))))))")
     refute @dpda_design.accepts?("())")
   end
+
+  def test_palindrome
+    rulebook = DPDARulebook.new([
+      PDARule.new(1, "a", 1, "$", ["a", "$"]),
+      PDARule.new(1, "a", 1, "a", ["a", "a"]),
+      PDARule.new(1, "a", 1, "b", ["a", "b"]),
+      PDARule.new(1, "b", 1, "$", ["b", "$"]),
+      PDARule.new(1, "b", 1, "a", ["b", "a"]),
+      PDARule.new(1, "b", 1, "b", ["b", "b"]),
+      PDARule.new(1, "m", 2, "$", ["$"]),
+      PDARule.new(1, "m", 2, "a", ["a"]),
+      PDARule.new(1, "m", 2, "b", ["b"]),
+      PDARule.new(2, "a", 2, "a", []),
+      PDARule.new(2, "b", 2, "b", []),
+      PDARule.new(2, nil, 3, "$", ["$"]),
+    ])
+    dpda_design = DPDADesign.new(1, "$", [3], rulebook)
+    assert dpda_design.accepts?("abmba")
+    assert dpda_design.accepts?("babbamabbab")
+    refute dpda_design.accepts?("abmb")
+    refute dpda_design.accepts?("baambaa")
+  end
 end
