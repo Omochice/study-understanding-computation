8c6fad29b4e948d6cd2c026b5e9b8e60d2f8bf6e
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-28 21:43:03 +0900

    :hammer: Add FIVE, FIFTEEN

--- src/lambda/fizzbuzz.rb
+++ src/lambda/fizzbuzz.rb
@@ -2,21 +2,14 @@ ZERO = ->p { ->x { x } }
 ONE = ->p { ->x { p[x] } }
 TWO = ->p { ->x { p[p[x]] } }
 THREE = ->p { ->x { p[p[p[x]]] } }
+FIVE = ->p { ->x { p[p[p[p[p[x]]]]] } }
+FIFTEEN = ->p { ->x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]] } }
+HANDRED = ->p { ->x { p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[p[x]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]] } }
 
 def to_integer(proc)
   return proc[->n { n + 1 }][0]
 end
 
 if $0 == __FILE__
-  (1..100).map do |n|
-    if (n % 15).zero?
-      return "FizzBuzz"
-    elsif (n % 3).zero?
-      return "Fizz"
-    elsif (n % 5).zero?
-      return "Buzz"
-    else
-      return n.to_s
-    end
-  end
+  puts to_integer()
 end
