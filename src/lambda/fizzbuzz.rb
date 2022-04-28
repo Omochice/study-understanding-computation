abcd1e65fe4421566175b25e3c286b604de969aa
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-28 21:36:14 +0900

    :hammer: Add translator for roc to ruby

--- src/lambda/fizzbuzz.rb
+++ src/lambda/fizzbuzz.rb
@@ -3,6 +3,10 @@ ONE = ->p { ->x { p[x] } }
 TWO = ->p { ->x { p[p[x]] } }
 THREE = ->p { ->x { p[p[p[x]]] } }
 
+def to_integer(proc)
+  return proc[->n { n + 1 }][0]
+end
+
 if $0 == __FILE__
   (1..100).map do |n|
     if (n % 15).zero?
