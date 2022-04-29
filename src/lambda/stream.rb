765ead54082bc384c8a805a9ca4357ae65f76940
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-30 01:39:09 +0900

    :hammer: Add `upwards_of` and ` multipy_of`

--- src/lambda/stream.rb
+++ src/lambda/stream.rb
@@ -1,5 +1,12 @@
 require_relative "./array.rb"
 require_relative "./combinator.rb"
+require_relative "./operators.rb"
 require_relative "./numbers.rb"
 
 ZEROS = Z[->f { UNSHIFT[f][ZERO] }]
+UPWARD_OF = Z[->f { ->n { UNSHIFT[->x { f[INCREMENT[n]][x] }][n] } }]
+MULTIPLY_OF = ->m {
+  Z[->f {
+      ->n { UNSHIFT[->x { f[ADD[m][n]][x] }][n] }
+    }][m]
+}
--- test/lambda/stream.rb
+++ test/lambda/stream.rb
@@ -12,4 +12,18 @@ class Stream < Minitest::Test
       zeros = REST[ZEROS]
     end
   end
+
+  def test_upwards_of
+    assert_equal((0...0 + 5).to_a,
+                 to_array(UPWARD_OF[ZERO], 5).map { |e| to_integer(e) })
+    assert_equal((15...15 + 20).to_a,
+                 to_array(UPWARD_OF[FIFTEEN], 20).map { |e| to_integer(e) })
+  end
+
+  def test_multiply_of
+    assert((2...2 + 10).map { |e| e * 2 },
+           to_array(MULTIPLY_OF[TWO], 10).map { |e| to_integer(e) })
+    assert((5...5 + 20).map { |e| e * 2 },
+           to_array(MULTIPLY_OF[FIVE], 20).map { |e| to_integer(e) })
+  end
 end
