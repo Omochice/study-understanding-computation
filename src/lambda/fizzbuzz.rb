cec85e50ba7c0856d542e76f163ba45d33871452
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-29 21:19:31 +0900

    :hammer: Add test for `ADD`, `SUBSTRACT`, `MULTIPLY`, `POWER`

--- src/lambda/fizzbuzz.rb
+++ src/lambda/fizzbuzz.rb
@@ -17,8 +17,13 @@ LEFT = ->p { p[->x { ->y { x } }] }
 RIGHT = ->p { p[->x { ->y { y } }] }
 
 INCREMENT = ->n { ->p { ->x { p[n[p][x]] } } }
-SLIDE = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
-DECREMENT = -> n {LEFT[n[SLIDE][PAIR[ZERO][ZERO]]]}
+SLIDE = ->p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
+DECREMENT = ->n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
+
+ADD = ->m { ->n { n[INCREMENT][m] } }
+SUBSTRACT = ->m { ->n { n[DECREMENT][m] } }
+MULTIPLY = ->m { ->n { n[ADD[m]][ZERO] } }
+POWER = ->m { ->n { n[MULTIPLY[m]][ONE] } }
 
 def to_integer(proc)
   return proc[->n { n + 1 }][0]
--- test/lambda/fizzbuzz.rb
+++ test/lambda/fizzbuzz.rb
@@ -69,4 +69,39 @@ class OperatorTest < Minitest::Test
     # assert_equal(-1, to_integer(DECREMENT[ZERO]))
     assert_equal(0, to_integer(DECREMENT[ZERO]))
   end
+
+  def test_add
+    assert_equal(0, to_integer(ADD[ZERO][ZERO]))
+    assert_equal(1, to_integer(ADD[ONE][ZERO]))
+    assert_equal(6, to_integer(ADD[ONE][FIVE]))
+    assert_equal(115, to_integer(ADD[HANDRED][FIFTEEN]))
+  end
+
+  def test_substract
+    assert_equal(0, to_integer(SUBSTRACT[ONE][ONE]))
+    assert_equal(1, to_integer(SUBSTRACT[ONE][ZERO]))
+    assert_equal(99, to_integer(SUBSTRACT[HANDRED][ONE]))
+  end
+
+  def test_multiply
+    # number * 1 = number
+    assert_equal(1, to_integer(MULTIPLY[ONE][ONE]))
+    assert_equal(100, to_integer(MULTIPLY[HANDRED][ONE]))
+    # number * 0 = 0
+    assert_equal(0, to_integer(MULTIPLY[ONE][ZERO]))
+    # multiply is commutative
+    assert_equal(to_integer(MULTIPLY[TWO][THREE]),
+                 to_integer(MULTIPLY[THREE][TWO]))
+  end
+
+  def test_power
+    # normal
+    assert_equal(8, to_integer(POWER[TWO][THREE]))
+    assert_equal(3125, to_integer(POWER[FIVE][FIVE])) # this is little slow
+    # power is not commutative
+    refute (to_integer(POWER[TWO][THREE]) \
+             == to_integer(POWER[THREE][TWO]))
+    # number ^ 0 is 1
+    assert_equal(1, to_integer(POWER[THREE][ZERO]))
+  end
 end
