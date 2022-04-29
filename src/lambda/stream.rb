1cb6a398d78f2420d2fdb5e2bba254223f06359b
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-04-30 01:44:18 +0900

    📛 Rename

--- src/lambda/stream.rb
+++ src/lambda/stream.rb
@@ -4,8 +4,8 @@ require_relative "./operators.rb"
 require_relative "./numbers.rb"
 
 ZEROS = Z[->f { UNSHIFT[f][ZERO] }]
-UPWARD_OF = Z[->f { ->n { UNSHIFT[->x { f[INCREMENT[n]][x] }][n] } }]
-MULTIPLY_OF = ->m {
+UPWARDS_OF = Z[->f { ->n { UNSHIFT[->x { f[INCREMENT[n]][x] }][n] } }]
+MULTIPLES_OF = ->m {
   Z[->f {
       ->n { UNSHIFT[->x { f[ADD[m][n]][x] }][n] }
     }][m]
--- test/lambda/stream.rb
+++ test/lambda/stream.rb
@@ -15,15 +15,15 @@ class Stream < Minitest::Test
 
   def test_upwards_of
     assert_equal((0...0 + 5).to_a,
-                 to_array(UPWARD_OF[ZERO], 5).map { |e| to_integer(e) })
+                 to_array(UPWARDS_OF[ZERO], 5).map { |e| to_integer(e) })
     assert_equal((15...15 + 20).to_a,
-                 to_array(UPWARD_OF[FIFTEEN], 20).map { |e| to_integer(e) })
+                 to_array(UPWARDS_OF[FIFTEEN], 20).map { |e| to_integer(e) })
   end
 
-  def test_multiply_of
+  def test_multiples_of
     assert((2...2 + 10).map { |e| e * 2 },
-           to_array(MULTIPLY_OF[TWO], 10).map { |e| to_integer(e) })
+           to_array(MULTIPLES_OF[TWO], 10).map { |e| to_integer(e) })
     assert((5...5 + 20).map { |e| e * 2 },
-           to_array(MULTIPLY_OF[FIVE], 20).map { |e| to_integer(e) })
+           to_array(MULTIPLES_OF[FIVE], 20).map { |e| to_integer(e) })
   end
 end
