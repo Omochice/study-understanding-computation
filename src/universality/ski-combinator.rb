343160501454af82bf46b29528e8cb35944a1f61
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-05-05 01:47:30 +0900

    :hammer: Add `callable?` method

--- src/universality/ski-combinator.rb
+++ src/universality/ski-combinator.rb
@@ -14,6 +14,10 @@ class SKISymbol < Struct.new(:name)
   def arguments
     return []
   end
+
+  def callable?(*_arguments)
+    return false
+  end
 end
 
 class SKICall < Struct.new(:left, :right)
@@ -43,10 +47,22 @@ def S.call(a, b, c)
   return SKICall.new(SKICall.new(a, c), SKICall.new(b, c))
 end
 
+def S.callable?(*arguments)
+  return arguments.length == 3
+end
+
 def K.call(a, _b)
   return a
 end
 
+def K.callable?(*arguments)
+  return arguments.length == 2
+end
+
 def I.call(a)
   return a
+end
+
+def I.callable?(*arguments)
+  return arguments.length == 1
 end
