717837c13290e8d7464050d604ff2e70a57f17da
mochi-MizLab <mochice.mls.ntl@gmail.com>
2022-06-06 02:19:50 +0900

    :hammer: Add `run` method into `TagSystem`

--- src/universality/tag-system.rb
+++ src/universality/tag-system.rb
@@ -16,10 +16,22 @@ class TagRuleBook < Struct.new(:deletion_number, :rules)
   def rule_for(string)
     return self.rules.detect { |r| r.applies_to?(string) }
   end
+
+  def applies_to?(string)
+    return !rule_for(string).nil? && string.length >= self.deletion_number
+  end
 end
 
 class TagSystem < Struct.new(:current_string, :rulebook)
   def step
     self.current_string = self.rulebook.next_string(self.current_string)
+  end
+
+  def run
+    while self.rulebook.applies_to?(current_string)
+      yield current_string
+      step
+    end
+    yield current_string
   end
 end
